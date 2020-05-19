0d577d0b4bc4cbe64576eb6fecb4208bcfbe802f
tanmoyopenroot <tanmoy.openroot@gmail.com>
2020-05-19 06:44:27 +0530

    fix(types): use types from common

--- src/install.ts
+++ src/install.ts
@@ -1,19 +1,27 @@
-import _Vue from 'vue';
+import _Vue, { RenderContext } from 'vue';
 import isFunction from '@/utils/is-function';
 import hyperscript from '@/hyperscript';
+import {
+  IVue,
+  ICreateElement,
+} from '../types/common';
+interface IOptions {
+  $options: {
+    renderHyperScript: (createElement: ICreateElement, ctx: RenderContext) => any;
+  };
+}
 
-type IVue = typeof _Vue;
-
-export default (Vue: any) => {
+export default (Vue: IVue) => {
   Vue.mixin({
-    render(h: any, ctx: any) {
-      const { renderHyperScript } = this.$options;
-      const hyper = hyperscript(h);
+    render(h, ctx) {
+      const { renderHyperScript } = (this as IOptions).$options;
 
       if (
         renderHyperScript &&
         isFunction(renderHyperScript)
       ) {
+        const hyper = hyperscript(h);
+
         return renderHyperScript.call(this, hyper, ctx);
       }
     },
